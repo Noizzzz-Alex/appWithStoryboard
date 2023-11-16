c3aa7c258a12292008e0f89c9a8daf900728bcb8
Noizzzz-Alex <Noizzzz@yandex.ru>
2023-11-17 03:25:38 +0700

    Initial Commit

---
+++ appWithStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appWithStoryboard
+//
+//  Created by Александр Харлампов on 17.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
